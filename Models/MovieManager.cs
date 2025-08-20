c11cc4d56d71cac2da9a465e12ce875a32ed54b5
Michael Nielsen <michael@halfnielsen.com>
2025-08-20 17:31:50 +0200

    File structure

---
+++ Models/MovieManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieOrganiser2000.Models
+{
+    public class MovieManager
+    {
+        private ObservableCollection<Movie> _DatabaseMovies = new ObservableCollection<Movie>()
+        { };
+
+        public ObservableCollection<Movie> GetMovies()
+        {
+            return _DatabaseMovies;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            _DatabaseMovies.Add(movie);
+        }
+    }
+}
