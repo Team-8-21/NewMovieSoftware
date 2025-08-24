84732d92a9278dd7d5cb5371e42c6224fd90cb28
Melinda Allen <msal71396@edu.ucl.dk>
2025-08-24 16:40:19 +0200

    Videre arbejde med MovieTheater

---
+++ Helpers/RelayCommandMovieTheater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MovieOrganiser2000.Helpers
+{
+    public class RelayCommandMovieTheater : ICommand
+    {
+        private readonly Action _executeMT;
+        private readonly Func<bool> _canExecuteMT;
+
+        public RelayCommandMovieTheater(Action execute, Func<bool> canExecute = null)
+        {
+            _canExecuteMT = canExecute;
+            _executeMT = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecuteMT?.Invoke() ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _executeMT();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
