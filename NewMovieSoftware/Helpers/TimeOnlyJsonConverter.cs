48c591908311bf295d40261f7309b134f35f22a3
Michael Nielsen <michael@halfnielsen.com>
2025-08-28 12:41:01 +0200

    Test Push

---
+++ NewMovieSoftware/Helpers/TimeOnlyJsonConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MovieOrganiser2000.Helpers
+{
+    public class TimeOnlyJsonConverter : JsonConverter<TimeOnly>
+    {
+        private const string TimeFormat = "HH:mm";
+
+        public override void WriteJson(JsonWriter writer, TimeOnly value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString(TimeFormat));
+        }
+
+        public override TimeOnly ReadJson(JsonReader reader, Type objectType, TimeOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            var value = reader.Value?.ToString();
+            if (string.IsNullOrEmpty(value))
+                return default;
+
+            if (TimeOnly.TryParse(value, out var time))
+                return time;
+
+            return default;
+        }
+    }
+}
